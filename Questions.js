ecc7f61f7cee7d14cd68d8bb7bcd9889536c1061
hritika <hritikasahu07@gmail.com>
2024-04-27 22:55:13 +0530

    initial release

---
+++ Questions.js
@@ -0,0 +1,58 @@
+let questions=[
+    {
+        numb:1,
+        question: "What does HTML stand for?",
+        answer:"C. Hyper text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper text Multiple Language",
+            "C. Hyper text Markup Language",
+            "D. Hyper text Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question: "What is HTML?",
+        answer:"d) All of the mentioned",
+        options: [
+            "a) HTML describes the structure of a webpage",
+            "b) HTML is the standard markup language mainly used to create web pages",
+            "c) HTML consists of a set of elements that helps the browser how to view the content",
+            "d) All of the mentioned"
+        ]
+    },
+    {
+        numb:3,
+        question: "Who is the father of HTML?",
+        answer:"b) Tim Berners-Lee",
+        options: [
+            "a) Rasmus Lerdorf",
+            "b) Tim Berners-Lee",
+            "c) Brendan Eich",
+            "d) Sergey Brin"
+        ]
+    },
+    {
+        numb:4,
+        question: "What attribute do we use for data binding?",
+        answer:"a. datasrc",
+    
+        options: [
+                "a. datasrc",
+                "b. mayscript",
+                "c. name",
+                "d. datafld"
+        ]
+    },
+    {
+        numb:5,
+        question: "Which of the following is used to read an HTML page and render it?",
+        answer:"c) Web browser",
+        options: [
+            "a) Web server",
+            "b) Web network",
+            "c) Web browser",
+            "d) Web matrix"
+        ]
+    }
+]
